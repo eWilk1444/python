cdbcc56b3cf6defe1f029eddd612e1083552462f
Ethan Wilkening <ewilkening@students.mchenry.edu>
2024-08-28 13:40:58 -0500

    Formatting Update

--- mathAssignment.py
+++ mathAssignment.py
@@ -3,8 +3,10 @@
 # Dimensions of rectangle inputs
 length = 12
 width = 7
+
+# Calculates area of rectangle based on inputs
 area = length * width
 
 # Prints calculation to console
-print("The area of the rectangle with a length of", str(
-    length), "and a width of", str(width), "is", str(area))
+print("\nThe area of the rectangle with a length of", str(
+    length), "and a width of", str(width), "is", str(area), "\n")
