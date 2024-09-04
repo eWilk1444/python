d4916f51e67d904c6952d05209ceb148bcb51d3d
eWilk1444 <ewilkening@students.mchenry.edu>
2024-09-04 14:13:31 -0500

    commit

--- conversion.py
+++ conversion.py
@@ -17,8 +17,9 @@ lbs_4 = kg_amount_4 * conversion_factor
 
 # Prints results to console for kg_amount_1 through kg_amount_4
 # Rounds to the second decimal place
-print("\nPlease note that these calculations are rounded to the second decimal place.")
-print(f"\n{kg_amount_1} kilogram(s) is about {lbs_1:.2f} pounds")
-print(f"{kg_amount_2} kilogram(s) is about {lbs_2:.2f} pounds")
-print(f"{kg_amount_3} kilogram(s) is about {lbs_3:.2f} pounds")
-print(f"{kg_amount_4} kilogram(s) is about {lbs_4:.2f} pounds", "\n")
+print("\nPlease note that these calculations are rounded to the second decimal place.\n")
+print(f"{kg_amount_1} kilogram(s) is about {lbs_1:.2f} pounds", "\n"
+      f"{kg_amount_2} kilogram(s) is about {lbs_2:.2f} pounds", "\n"
+      f"{kg_amount_3} kilogram(s) is about {lbs_3:.2f} pounds", "\n"
+      f"{kg_amount_4} kilogram(s) is about {lbs_4:.2f} pounds", "\n"
+      )
