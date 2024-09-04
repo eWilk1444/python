2a59ee970995197c47b510dbe43372f5c520a60c
Ethan Wilkening <ewilkening@students.mchenry.edu>
2024-09-04 13:36:23 -0500

    upload

---
+++ comments.py
@@ -0,0 +1,12 @@
+# This file shows different types of comments
+"""
+    Type the above code into Visual Studio Code
+    Add a minimum of 4 comments to the above Python 
+    program to make it more understandable.
+"""
+
+tank_size = 15  # amount of gallons of liquid in a given gas tank
+distance = 600  # distance in miles that the vehicle travels
+# calculates how many miles each gallon will move the vehicle
+mpg = distance / tank_size
+print("Miles per gallon:", mpg)
