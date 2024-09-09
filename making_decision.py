a738e3a118cde7b68c574673b2206ba45bfe903b
eWilk1444 <ewilkening@students.mchenry.edu>
2024-09-09 14:41:50 -0500

    upload

--- making_decision.py
+++ making_decision.py
@@ -4,3 +4,25 @@
 """
 
 # variables
+age = input("How old are you? ")
+senior = False
+alchohol = False
+vote = False
+drive = False
+
+print(senior)
+
+# logic
+if int(age) >= 65:
+    senior = True
+elif age >= 21:
+    alchohol = True
+elif age >= 18:
+    vote = True
+elif age >= 16:
+    drive = True
+else:
+    senior = False
+    alchohol = False
+    vote = False
+    drive = False
