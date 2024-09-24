cfe53b9fce0bfb463f7fea779da37ecd6503e8df
eWilk1444 <ewilkening@students.mchenry.edu>
2024-09-23 20:45:51 -0500

    upload

--- projects/integer_compare.py
+++ projects/integer_compare.py
@@ -9,15 +9,38 @@ num1 = int(input("Please enter an integer: "))
 num2 = int(input("\nPlease enter another integer: "))
 
 print("Are both integers less than 0? ")
-if num1 and num2 > 0:
+if num1 < 0 and num2 < 0:
     print("True")
 else:
     print("False")
 
-# if num1 and num2 % 2 == 0
+print("are both integers even? ")
+if num1 % 2 == 0 and num2 % 2 == 0:
+    print("True")
+else:
+    print("False")
+# got some help from Chat GPT https://chatgpt.com/share/66f2192b-1c1c-800f-8964-8585c047bace
 
-# if num1 or num2 == 42
+print("Are either integers the number 42? ")
+if num1 == 42 or num2 == 42:
+    print("True")
+else:
+    print("False")
 
-# if num1 or num2 < 1000
+print("Are either integers less than 1000? ")
+if num1 < 1000 or num2 < 1000:
+    print("True")
+else:
+    print("False")
 
-# if num1 not num2 == num2
+print("Are the integers the same? ")
+if not num1 == num2:
+    print("False")
+else:
+    print("True")
+
+print("Is the first integer larger than the second, but also larger than 3? ")
+if not num1 > num2 > 3:
+    print("True")
+else:
+    print("False")
