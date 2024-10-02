a88646979db5e3c8480f8264b6e48ee81ff5dcd8
eWilk1444 <ewilkening@students.mchenry.edu>
2024-10-02 14:24:06 -0500

    upload

--- projects/madlibs.py
+++ projects/madlibs.py
@@ -1,5 +1,26 @@
 """
     It's madlibs, iykyk
 """
-# If I had a million dollars by Barenaked Ladies
-def custom_song(noun, verb_1, verb_2, )
+# Opening chorus of 'Chop Suey' by System of a Down
+def custom_song(verb1, verb2, noun1, noun2)
+
+
+# Wake up(wake up)
+# {verb} a {noun1} and put a little make-up
+# {verb2} the scars to fade away the shake-up({verb2} the scars to fade away the-)
+# Why'd you leave the {noun2} upon the table?
+# Here you go create another fable, you wanted to
+# {verb} a {noun1} and put a little make-up, you wanted to
+# {verb2} the scars to fade away the shake-up, you wanted to
+# Why'd you leave the {noun2} upon the table? You wanted to
+# I don't think you trust
+# In my self-righteous suicide
+# I cry when angels deserve to die
+# Wake up (wake up)
+# {verb} a {noun1} and put a little make-up
+# {verb2} the scars to fade away the ({verb2} the scars to fade away the shake-up)
+# Why'd you leave the {noun2} upon the table?
+# Here you go create another fable, you wanted to
+# {verb} a {noun1} and put a little make-up, you wanted to
+# {verb2} the scars to fade away the shake-up, you wanted to
+# Why'd you leave the {noun2} upon the table? You wanted to
