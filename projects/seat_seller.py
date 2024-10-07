32c4343740aa2b545068e2125ac57931d9c4552a
eWilk1444 <ewilkening@students.mchenry.edu>
2024-10-07 13:03:38 -0500

    upload

--- projects/seat_seller.py
+++ projects/seat_seller.py
@@ -1,5 +1,6 @@
 """
     Creates a list of 1-20 seats, and prompts user to select seats to purchase
+    https://chatgpt.com/share/66f45d4c-d1c0-800f-a4fe-60fec9e8e4aa
 """
 
 available_seats = [1, 2, 3, 4, 5, 6, 7, 8, 9,
@@ -17,12 +18,6 @@ while len(available_seats) > 0 and wanted_seat != 0:
         all_wanted_seats.append(wanted_seat)
         print(f"\nYou have reserved seat {
               wanted_seat}. Enter 0 if you are finished with your purchase.")
-    # if wanted_seat in available_seats:
-    #     available_seats.remove(wanted_seat)
-    #     all_wanted_seats.append(wanted_seat)
-    #     print(f"\nYou have reserved seat {
-    #           wanted_seat}. Enter 0 if you are finished with your purchase.")
-    # https://chatgpt.com/share/66f45d4c-d1c0-800f-a4fe-60fec9e8e4aa
     elif wanted_seat == 0:
         print(f"\nThe seat(s) you have reserved are: {all_wanted_seats}.")
         print("\nThank you for your patronage.\n")
