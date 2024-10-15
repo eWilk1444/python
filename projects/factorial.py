c2db0f0155f0a0ca5fc893132d0b3e6145bae00b
eWilk1444 <ewilkening@students.mchenry.edu>
2024-10-14 20:30:54 -0500

    upload

--- projects/factorial.py
+++ projects/factorial.py
@@ -2,18 +2,22 @@
     Calculates factorials of numbers inputted by user
 """
 
-
-def factorial_calculate(n):
-    if n <= 0:
-        return n
+# calculates factorial using a recursive loop
+def calculate_factorial(number):
+    if number < 0:
+        return number
+    elif number == 0:
+        return 1
     else:
-        return n * factorial_calculate(n-1)
+        return (number * calculate_factorial(number - 1))
 
-
+# main logic invokes calculate_factorials function, prints results to terminal
 def main():
-    number = int(input("\nPlease enter an integer to calculate its factorial. "))
-    total = factorial_calculate(number)
-    print(f"\nThe factorial of {number} is {total}. ")
+    number = int(input("\nPlease enter a integer to find the factorial of. "))
+    total = calculate_factorial(number)
+    print(f"\nThe factorial of {number} is {total}. \n")
+# https://chatgpt.com/share/670dc529-49d4-800f-8b40-e131e5385f03 ChatGPT loves to write code, need to tell it everytime to not do it!
+# It did remind me to store data in the main function, so that was helpful.
 
-
+# invoking main function
 main()
