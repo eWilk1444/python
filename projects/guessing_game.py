208789dab1f60e43a42d946301fc0700a0f2836a
eWilk1444 <ewilkening@students.mchenry.edu>
2024-10-23 14:19:31 -0500

    upload

--- projects/guessing_game.py
+++ projects/guessing_game.py
@@ -3,34 +3,30 @@
 """
 
 import random
-
-
-def number_generator():
-    number = random.randint(0, 101)
-    return number
+global GEN_NUMBER
+GEN_NUMBER = random.randint(0, 101)
 
 
 def user_input():
     try:
         guessed_number = int(
-            input("\nGuess a number between 1 and 100, including 1 and 100. "))
+            input("\nGuess a number between 1 and 100, including 1 and 100: "))
     except ValueError:
-        print(f"\nError! Please enter a whole number. ")
+        print(f"\nError! Please enter a whole number: ")
         user_input()
     except TypeError as t:
-        print(f"\nError! Please enter a number. ")
+        print(f"\nError! Please enter a number: ")
         user_input()
     else:
         return guessed_number
 
 
 def main():
-    gen_number = number_generator()
     guessed_number = user_input()
-    distance_from_guess = abs(guessed_number - gen_number)
+    distance_from_guess = abs(guessed_number - GEN_NUMBER)
 
     if distance_from_guess == 0:
-        print(f"\nCongrats! {gen_number} was the number!")
+        print(f"\nCongrats! {guessed_number} was the number!\n")
     elif distance_from_guess <= 5:
         print("\nVery Hot!")
         main()
