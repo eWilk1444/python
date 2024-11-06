a3a7bf546ea2d0a9e1fbffb782c71a1d01b5983f
eWilk1444 <ewilkening@students.mchenry.edu>
2024-11-06 12:55:25 -0600

    upload

---
+++ projects/password_validator.py
@@ -0,0 +1,16 @@
+"""
+Python program that prompts the user to enter a password and validates it against specific criteria
+
+Set up your program in a main() function
+Create a Python program that asks the user to input a password.
+Ensure the password meets the following criteria:
+Between 8 to 20 characters long.
+Contains at least one uppercase letter.
+Contains at least one lowercase letter.
+Includes at least one number.
+Includes at least one symbol from the set: !@#$%&*.
+Use a while loop to keep asking for the password until all criteria are met.
+Once a valid password is entered, prompt the user to enter it again for confirmation.
+Use try-except blocks to handle any errors or exceptions that occur.
+If the second password entry matches the first, display a success message. Otherwise, prompt the user to start the process again.
+"""
