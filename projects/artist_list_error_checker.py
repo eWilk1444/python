bfbf5bc44f89bc1c62407ebe6b38d37153dcfeb4
eWilk1444 <ewilkening@students.mchenry.edu>
2024-11-06 13:00:04 -0600

    upload

---
+++ projects/artist_list_error_checker.py
@@ -0,0 +1,6 @@
+"""
+sPython program that handles exceptions related to list operations
+
+Modify Artist List: Write a function to replace an artist in the top_artists list. The function should ask the user for an index and a new artist name. Ensure your function catches and handles ValueError for invalid number conversion and IndexError for out-of-range indices.
+General Error Handling: Modify your function to catch both ValueError and IndexError using a single except block. Provide a general error message like "An input error occurred."
+    """
