9509fdd6d71d96f01307b893ac9da7d9c9ff0a85
eWilk1444 <ewilkening@students.mchenry.edu>
2024-11-06 12:58:40 -0600

    upload

---
+++ projects/flower_list_manager.py
@@ -0,0 +1,11 @@
+"""
+Manage a list of flowers. This includes sorting the list, searching for specific flowers, and handling exceptions using try and except statements.
+
+Create a Main Function: Encapsulate your program within a main() function.
+User Input for Flower List: Prompt the user to enter names of flowers and store them in a list. Have them enter the word "done" when done, and check for it.
+Sorting and Searching: Sort the list, print on screen with a number next to the flower name, and allow the user to search for a specific flower. Print a message if the flower is found.
+Accessing a Specific Flower: Ask the user for a number to access the corresponding flower. Handle errors for invalid inputs. (Note, our printout starts at 1, list indexes start at 0, adjust accordingly.
+Exception Handling: Use try and except statements for ValueError and IndexError, and include a generic except statement.
+Upload to GitHub: Upload your completed script to your GitHub repository.
+Submission: Submit the GitHub repository link containing your script.
+"""
