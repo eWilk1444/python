6821b3811fda89a66766514621159f3ccd41bd6c
eWilk1444 <ewilkening@students.mchenry.edu>
2024-11-18 13:43:24 -0600

    upload

---
+++ projects/employee_classes.py
@@ -0,0 +1,73 @@
+"""
+Assignment Part 1: Defining Classes
+File 1: Write an Employee class with the following attributes:
+
+Employee name
+Employee number
+Create a subclass named ProductionWorker that inherits from Employee. This subclass should include additional attributes:
+
+Shift number (integer: 1 for day, 2 for night)
+Hourly pay rate
+Implement accessor and mutator methods (getters and setters) for each class.
+
+Assignment Part 2: Implementing and Testing
+ 
+
+Part 2: Write a script to:
+
+Create an instance of ProductionWorker.
+Prompt the user for each attribute's data.
+Store and then display the data using the object's methods.
+"""
+
+
+class Employee:
+    def __init__(self, name, number):
+        self.__name = name
+        self.__number = number
+
+    # generated using Python Getter Setter
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def number(self):
+        return self.__number
+
+    @number.setter
+    def number(self, value):
+        self.__number = value
+
+
+class Production_Worker:
+    def __init__(self, name, number, shift, pay_rate):
+        super().__init__(name, number)
+        self.__shift = shift
+        self.__pay_rate = pay_rate
+
+    @property
+    def shift(self):
+        return self.__shift = shift
+
+    @shift.setter
+    def shift(self, value):
+        self.__shift = value
+
+    @property
+    def pay_rate(self):
+        return self.__pay_rate
+
+    @pay_rate.setter
+    def pay_rate(self, value):
+        self.__pay_rate = value
+
+
+def main():
+
+
+main()
